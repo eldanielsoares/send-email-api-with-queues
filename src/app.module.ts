54c72079e1b3cbdeaf7a3bbafc6815c74ce386fb
eldanielsoares <danielsoares7717@gmail.com>
2024-12-18 14:30:15 -0300

    ferat: add app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,34 @@
+import { BullModule } from '@nestjs/bull';
 import { Module } from '@nestjs/common';
+import { SendEmailsModule } from './modules/send-emails/send-emails.module';
+import { ConfigModule } from '@nestjs/config';
+import { MailerModule } from '@nestjs-modules/mailer';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    BullModule.forRoot({
+      redis: {
+        host: process.env.REDIS_HOST,
+        port: Number(process.env.REDIS_PORT),
+      },
+    }),
+    MailerModule.forRoot({
+      transport: {
+        host: process.env.MAILER_HOST,
+        port: Number(process.env.MAILER_PORT),
+        secure: false,
+        auth: {
+          user: process.env.MAILER_USER,
+          pass: process.env.MAILER_PASS,
+        },
+        tls: {
+          rejectUnauthorized: false,
+        },
+      },
+    }),
+    SendEmailsModule,
+  ],
   controllers: [],
   providers: [],
 })
